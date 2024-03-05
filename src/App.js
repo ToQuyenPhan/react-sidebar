94e11665d0a26659ba93ac2283bd19466cfa7b8f
ToQuyenPhan <duonglh024@gmail.com>
2024-03-06 04:57:36 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Sidebar from './components/Sidebar';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Reports from './pages/Reports';
+import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUs';
+import Support from './pages/Support';
 
 function App() {
   return (
@@ -8,7 +14,12 @@ function App() {
       <Router>
         <Sidebar />
         <Routes>
-          <Route path='/' />
+          <Route path='/' exact element={<Home />}/>
+          <Route path='/products' element={<Products />}/>
+          <Route path='/reports' element={<Reports />}/>
+          <Route path='/about-us' element={<AboutUs />}/>
+          <Route path='/contact-us' element={<ContactUs />}/>
+          <Route path='/support' element={<Support />}/>
         </Routes>
       </Router>
     </>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,9 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { HiBars3BottomLeft } from "react-icons/hi2";
 import { IoIosCloseCircle } from "react-icons/io";
 import { SidebarData } from './SidebarData';
+import { IconContext } from 'react-icons'
 import './Sidebar.css';
 
 function Sidebar() {
@@ -12,32 +13,34 @@ function Sidebar() {
 
     return (
         <>
-            <div className="navbar">
-                <Link to="" className='menu-bars'>
-                    <HiBars3BottomLeft onClick={showSidebar}/>
-                </Link>
-            </div>
-            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-                <ul className='nav-menu-items'>
-                    <li className="navbar-toggle">
-                        <Link to='#' className='menu-bars'>
-                            <IoIosCloseCircle />
-                        </Link>
-                    </li>
-                    {
-                        SidebarData.map((item, index) => {
-                            return (
-                                <li key={index} className={item.cName}>
-                                    <Link to={item.path}>
-                                        {item.icon}
-                                        <span>{item.title}</span>
-                                    </Link>
-                                </li>
-                            )
-                        })
-                    }
-                </ul>
-            </nav>
+            <IconContext.Provider value={{color: '#fff'}}>
+                <div className="navbar">
+                    <Link to="#" className='menu-bars'>
+                        <HiBars3BottomLeft onClick={showSidebar} />
+                    </Link>
+                </div>
+                <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+                    <ul className='nav-menu-items' onClick={showSidebar}>
+                        <li className="navbar-toggle">
+                            <Link to='#' className='menu-bars'>
+                                <IoIosCloseCircle />
+                            </Link>
+                        </li>
+                        {
+                            SidebarData.map((item, index) => {
+                                return (
+                                    <li key={index} className={item.cName}>
+                                        <Link to={item.path}>
+                                            {item.icon}
+                                            <span>{item.title}</span>
+                                        </Link>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                </nav>
+            </IconContext.Provider>
         </>
     )
 }
